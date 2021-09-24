1a2b515b5b54d8d6f3f504673adf40046598da0f
parth butani <parthb401@gmail.com>
2021-09-24 15:18:47 +0530

    Update Db config

--- src/db/mongoose.config.js
+++ src/db/mongoose.config.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(`mongodb://${process.env.MONGO_URL}/${process.env.MONGO_DB}`, {
+mongoose.connect("mongodb://127.0.0.1:27017/node_demo_project", {
 	useNewUrlParser: true,
 	// useCreateIndex: true,
 	useUnifiedTopology: true,
